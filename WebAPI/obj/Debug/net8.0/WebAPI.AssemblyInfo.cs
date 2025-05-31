115ee39bb4e7b21fd00f5c9ea06929e6ae91fb92
grouab <abderrahmane.grou@polymtl.ca>
2025-05-31 17:28:23 -0400

    First EF Migration

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27967512d45964cab587e4fa5a1c744e08c4b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ae8318ba7ff45c169ea4944a5ebac6705d56a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
